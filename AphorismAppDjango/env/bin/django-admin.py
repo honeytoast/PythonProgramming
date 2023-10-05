7853dd88a7f661d9c0e725f8c04134f476e78a9c
Grace Hadiyanto <ifoundparis@gmail.com>
2023-10-05 00:04:50 -0700

    Delete AphorismAppDjango directory

--- AphorismAppDjango/env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/honeytoast/Documents/Python/Hadiyanto.Grace_asgt7/aphorism_app/env/bin/python3.3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- AphorismAppDjango/env/lib/python3.3/sre_compile.py
+++
@@ -1,1 +0,0 @@
-/opt/local/Library/Frameworks/Python.framework/Versions/3.3/lib/python3.3/sre_compile.py
